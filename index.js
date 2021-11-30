dfbc6a5dd6d06e91e5400de777502315c10443d0
Austin <austinlongocontracting@gmail.com>
2021-11-30 17:35:37 -0500

    commit tree 71018cfb3441c034f41c562a33a4846fb93f590b

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+const multiply = num1 * num2;
+
+let random = Math.ceil(Math.random()) * Math.ceil(Math.random()) + 1;
+
+let num3 = 24;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,20);
